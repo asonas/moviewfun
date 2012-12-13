f9df65ea0d7b404213325750e45b933dceb8f1d0
asonas <hzw1258@gmail.com>
2012-12-14 06:20:50 +0900

    Add url column

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121213191750) do
+ActiveRecord::Schema.define(:version => 20121213211607) do
 
   create_table "accounts", :force => true do |t|
     t.integer  "user_id"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(:version => 20121213191750) do
     t.string   "description"
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
+    t.string   "url"
   end
 
   create_table "users", :force => true do |t|
