435c0a5f687d9b9a5a99747b52849f3e575a9f51
jigsaw <jigsaw@live.jp>
2012-12-18 01:29:17 +0900

    Fix get user and users movie query for #9

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,8 +2,8 @@ class UsersController < ApplicationController
   # GET /user/neo6120
   # GET /user/neo6120.json
   def show
-    @user = User.where(:id => params[:name]).first()
-    @movies = Movie.where(:user_id => params[:id])
+    @user = User.where(:name => params[:id]).first()
+    @movies = Movie.where(:user_id => @user.id)
 
     respond_to do |format|
       format.html #show.html.erb
